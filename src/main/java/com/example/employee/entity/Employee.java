020683cae8458d1392fa9997ac62d1cd62f79ec5
ctc <1171332103@qq.com>
2018-05-06 12:19:50 +0800

    提交结果

--- src/main/java/com/example/employee/entity/Employee.java
+++ src/main/java/com/example/employee/entity/Employee.java
@@ -1,5 +1,81 @@
 package com.example.employee.entity;
 
-public class Employee {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
 
+@Entity
+@Table(name="employee")
+public class Employee implements Serializable{
+
+    String name;
+    int age;
+    String gender;
+    @Id
+    @GeneratedValue
+    int id;
+    int companyId;
+    int salary;
+
+    public Employee() {
+    }
+
+    public Employee(String name, int age, String gender, int id, int companyId, int salary) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.id = id;
+        this.companyId = companyId;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(int companyId) {
+        this.companyId = companyId;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
 }
--- src/main/java/com/example/employee/repository/EmployeeRepository.java
+++ src/main/java/com/example/employee/repository/EmployeeRepository.java
@@ -1,7 +1,13 @@
 package com.example.employee.repository;
 
 import com.example.employee.entity.Employee;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -9,16 +15,28 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     //以下所有的*都代表变量
 
     //1.查询名字是*的第一个employee
-
+    @Query("select new com.example.employee.entity.Employee(em.name, em.age, em.gender, em.id, em.companyId, em.salary) from Employee as em where name=:name")
+    Employee findEmployeeByName(@Param("name")String name);
     //2.找出Employee表中第一个姓名包含`*`字符并且薪资大于*的雇员个人信息
-
+    @Query("select name from Employee where name like %:n% and salary > 6000")
+    String findEmployeeContainParam(@Param("n")String param);
     //3.找出一个薪资最高且公司ID是*的雇员以及该雇员的姓名
-
+    @Query("select name from Employee where id =:id  group by id")
+    String findMaxSalaryEmplaoyee(@Param("id")int id);
     //4.实现对Employee的分页查询，每页两个数据
-
+    @Query("select new com.example.employee.entity.Employee(em.name, em.age, em.gender, em.id, em.companyId, em.salary) from Employee as em")
+    Page<Employee> findWithPage(Pageable pageable);
     //5.查找**的所在的公司的公司名称
-
+    @Query("select companyName from Company where id = (select companyId from Employee where name=:name)")
+    String findCompanyName(@Param("name") String employeeName);
     //6.将*的名字改成*,输出这次修改影响的行数
-
+    @Modifying
+    @Query("update Employee set name=:newName where name=:name")
+    void updateNameToNew(@Param("name")String name, @Param("newName")String newName);
+    @Query(value = "select row_count()", nativeQuery = true)
+    int findAffectRows();
     //7.删除姓名是*的employee
+    @Modifying
+    @Query("delete from Employee where name=:name")
+    void deleteEmployeeByName(@Param("name")String name);
 }
